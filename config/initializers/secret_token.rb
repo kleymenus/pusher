709e092bb4ab2c360bc28979736e9c8162e10227
Alexander Kleymenov <unit@hasmind.net>
2013-04-02 12:33:08 +0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Push::Application.config.secret_token = '11722bc3ed36af5170e449df07c952b30a6f122c0b5623526cd2f6a5d2cf39f6750b7499b341fd82996ac8a64c3bf9bf99dd8f0d9ffca382afb878f533bca847'
